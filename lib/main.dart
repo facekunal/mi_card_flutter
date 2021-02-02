805e4c8a97435990310bc7d8ae34459db542e72d
Kunal Kotiyal <facekunal@gmail.com>
2021-02-02 22:19:18 +0530

    init project

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,80 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("assets/profile_image.jpg"),
+                backgroundColor: Colors.white,
+              ),
+              Text(
+                "Kakashi Hatake",
+                style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "LEAF SHINOBI",
+                style: TextStyle(
+                  fontFamily: "SourceSansPro",
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  thickness: 2.0,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                        "+91 9297453788",
+                        style: TextStyle(
+                          fontFamily: "SourceSansPro",
+                          color: Colors.teal.shade800,
+                          fontSize: 20.0,
+                        )
+                    ),
+                  )
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                        "kakashihatake@leaf.com",
+                        style: TextStyle(
+                          fontFamily: "SourceSansPro",
+                          color: Colors.teal.shade800,
+                          fontSize: 20.0,
+                        )
+                    ),
+                  )
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
